1702e5070d11826586ac94b0577a364e9ea11fb0
SHIM-JINSEO <sjs011828@gmail.com>
2026-01-10 17:51:06 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen flex justify-center items-center h-screen">
+      <h1 className="text-blue-600">Hi, I'm Jinseo</h1>
+    </div>
+  );
+}
+
+export default App;
