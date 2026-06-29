766af075907491f9cfd7a8a7a1eb83eb86b24df4
SHIM-JINSEO <jinseo5892@gm.gist.ac.kr>
2026-06-29 13:35:06 +0900

    fix: fix type error interrupting deploy

--- src/routes/FunFacts/index.tsx
+++ src/routes/FunFacts/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { Smile, Coffee, Compass, Sparkles, BookOpen, Atom } from "lucide-react";
+import { Smile, Coffee, Compass, BookOpen, Atom } from "lucide-react";
 
 export const Route = createFileRoute("/FunFacts/")({
   component: RouteComponent,
